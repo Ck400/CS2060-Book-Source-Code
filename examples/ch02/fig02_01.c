2c9d41e99c027d40edc895561f3bf5de6cabe4a8
Ck <cbergero@uccs.edu>
2022-06-20 16:16:28 -0600

    Done

--- examples/ch02/fig02_01.c
+++ examples/ch02/fig02_01.c
@@ -1,13 +1,43 @@
 // Fig. 2.1: fig02_01.c
 // A first program in C 
 #include <stdio.h>
+#include "fig02_01.h"
+
+int askUser(void);
 
 // function main begins program execution 
 int main( void )
 {
-   printf( "Welcome to C!\n" );
+	askUser();
+	
+	return 0;
 } // end function main 
 
+int askUser( void )
+{
+	int length = 1;
+	int width = 1;
+	int area = 1;
+	int division = 1;
+
+	// Ask user for length
+	puts("Enter the length");
+	scanf("%d", &length);
+
+	//Ask user for width
+	puts("Enter the width");
+	scanf("%d", &width);
+
+	//Assign variables
+	area = length * width;
+	division = length / width;
+
+	//Print out statements to user
+	printf("Length: %d Width: %d and Area is %d.\n", length, width, area);
+	printf("Length divided by width is %d", division);
+
+	return 0;
+}
 
 /**************************************************************************
  * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
